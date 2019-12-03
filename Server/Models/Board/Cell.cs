96072f68cb10ec12ccdcfca2af8a6d0fb8d87566
Michal Nabielec <michal.nabi@gmail.com>
2019-12-03 14:50:47 +0100

    Add board models

---
+++ Server/Models/Board/Cell.cs
@@ -0,0 +1,26 @@
+﻿namespace BattleShip.Models.Board
+{
+    public class Cell
+    { 
+        public Coordinates Coordinates { get; set; }
+        public OccupationTypeField OccupationTypeField { get; set; }
+
+        public Cell(int row, int column)
+        {
+            this.Coordinates = new Coordinates(row, column);
+            this.OccupationTypeField = OccupationTypeField.Empty;
+        }
+
+
+        public bool IsOccupied()
+        {
+            return
+                OccupationTypeField == OccupationTypeField.Destroyer ||
+                OccupationTypeField == OccupationTypeField.Aircraft ||
+                OccupationTypeField == OccupationTypeField.Battleship ||
+                OccupationTypeField == OccupationTypeField.Cruiser ||
+                OccupationTypeField == OccupationTypeField.Submarine;
+        }
+
+    }
+}
---
+++ Server/Models/Board/GameBoard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Channels;
+
+namespace BattleShip.Models.Board
+{
+    public class GameBoard
+    {
+        public List<Cell> Board { get; set; }
+
+        public GameBoard()
+        {
+            this.Board = new List<Cell>();
+
+            for (var row = 1; row <= 10; row++)
+            {
+                for ( var column = 1; column <= 10; column++)
+                {
+                    Board.Add(new Cell(row,column));
+                }
+            }
+        }
+    }
+}
