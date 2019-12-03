792af27f99c35ba83140545122de0ec03b9d9dcf
Michal <michal.nabie@gmail.com>
2019-12-03 20:45:46 +0100

    fetch data

--- Server/ClientApp/src/Components/Board.js
+++ Server/ClientApp/src/Components/Board.js
@@ -1,44 +1,33 @@
 import React, {Component} from "react";
 import axios from "../axios/axios";
 import Square from "./Square";
-export default class Board extends Component {
-    
-    constructor(props){
-        super(props);
-    }
 
+export default class Board extends Component {
     state = {
         squares: []
     };
 
-  componentDidMount() {
-      axios.get('/board')
-          .then(function (response) {
-              // handle success
-              console.log(response);
-          })
-          .catch(function (error) {
-              // handle error
-              console.log(error);
-          })
-          .finally(function () {
-              // always executed
-          });
-  }
+    componentDidMount() {
+        axios.get('/board')
+            .then(response => {
+                this.setState({squares: response.data.board})
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+    }
 
 
     render() {
-
-
-        return(
-
+        return (
             this.state.squares.map(square =>
                 <Square
-                    square={square}
-                key={square.id}/>
+                    square={square}/>
             )
-        );
-
+        )
     }
-
 }
