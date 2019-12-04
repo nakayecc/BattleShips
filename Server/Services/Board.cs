9636bee690989701a44c5f664d0b7e9f361ff2a2
Michal Nabielec <michal.nabi@gmail.com>
2019-12-04 10:24:23 +0100

    Add board

---
+++ Server/Services/Board.cs
@@ -0,0 +1,21 @@
+﻿using Server.Models.Board;
+
+namespace Server.Services
+{
+    public class Board
+    {
+
+        public GameBoard GameBoard { get; set; }
+
+        public void InitBoard()
+        {
+            this.GameBoard = new GameBoard();
+        }
+
+        public void ChangeSquare(Square square, int squareIndex)
+        {
+            GameBoard.Board[squareIndex] = square;
+        }
+
+    }
+}
