6c14714c42482dd680468589774aa94c142910ba
Michal Nabielec <michal.nabi@gmail.com>
2019-12-05 15:18:39 +0100

    Add player and enemy controller

---
+++ Server/Controllers/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models.Board;
+using Server.Services;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowMyOrigin")]
+    public class PlayerController : ControllerBase
+    {
+        private readonly IGame game;
+
+        public PlayerController(IGame game)
+        {
+            this.game = game;
+        }
+
+        // GET: api/Player
+        [HttpGet]
+        public Player Get()
+        {
+            return game.Player1;
+        }
+
+        // GET: api/Player/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Player
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Player/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
